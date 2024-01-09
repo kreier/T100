6f4f089f5ca242a53a9a0b4c45943cfc40a755bb
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2024-01-09 14:02:00 +0700

    udated to work with BlueDuino

--- T100_2024/T100_2024.ino
+++ T100_2024/T100_2024.ino
@@ -9,7 +9,7 @@ AF_DCMotor motor1(1, MOTOR12_64KHZ); // create motor #1, 64KHz pwm
 AF_DCMotor motor4(4, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
 SoftwareSerial BTSerial(A0, 3);   // RX | TX  -  pin 2 creates errors on my motor shield, analog pin is fine ...
 char BTinput = '0';
-byte speed = 200;
+int speed = 200;
 
 void setup() {
   motor1.setSpeed(100);
@@ -23,44 +23,45 @@ void loop() {
   if (BTSerial.available()) 
   {
     BTinput = BTSerial.read();
-    if (BTinput == 'F')      // forward  - A
+    if (BTinput == 'w')      // forward  - A
     {
       motor1.run(FORWARD);
       motor4.run(FORWARD);
     }
-    if (BTinput == 'B')      // backward - C
+    if (BTinput == 's')      // backward - C
     {
       motor1.run(BACKWARD);
       motor4.run(BACKWARD);
     }
-    if (BTinput == 'L')      // left     - D
+    if (BTinput == 'd')      // left     - D
     {
       motor1.run(FORWARD);
       motor4.run(BACKWARD);
     }
-    if (BTinput == 'R')      // right    - B
+    if (BTinput == 'a')      // right    - B
     {
       motor1.run(BACKWARD);
       motor4.run(FORWARD);
     }      
-    if (BTinput == 'X')      // that's the "X" key      - G
+    if (BTinput == 'b')      // that's the "X" key      - G
     {
       motor1.run(RELEASE);      // stopped
       motor4.run(RELEASE);
     }
-    if (BTinput == 'T')  // turbo, fastest - triangle   - E
+    if (BTinput == 'x')  // faster - plus 10  - circle  - F
     {
       speed = speed + 10;
     }
-    if (BTinput == 'Q')  // slower - minus 10 - square  - H
+    if (BTinput == 'c')  // slower - minus 10 - square  - H
     {
       speed = speed - 10;
     } 
-    if (BTinput == 'C')  // faster - plus 10  - circle  - F
+    if (BTinput == 'v')  // turbo, fastest - triangle   - E
     {
       speed = 255;
     }
     if( speed > 255 ) speed = 255;
+    if( speed < 0   ) speed = 0;
     motor1.setSpeed(speed);
     motor4.setSpeed(speed);
     Serial.print(millis());
